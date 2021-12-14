89da616e632b6bab546a32e891633eb1c9429231
Ben <92675290+ben-wheeler@users.noreply.github.com>
2021-12-14 13:36:26 +1030

    Breath working

--- BreadthFirstSearch.cpp
+++ BreadthFirstSearch.cpp
@@ -3,90 +3,22 @@
 
 using namespace std;
 
-// class for Nodes
-class Node{
-public:
-    int data;    // the value of each node
-    Node *next;  // pointer to next node
-
-    // constructor of new nodes
-    Node(int value){
-        data = value; // sets Nodes data to be equal to value passed in
+void printVec(vector<int> input)
+{
+    for (int i = 0; i < input.size(); i++)
+    {
+        cout << input[i] << ",";
     }
-}; // end class initialisation
-
-// structure for queue declaration
-struct Queue{
-    Node * head; // pointer to head
-    Node * tail; // pointer to tail
-    Queue(){ // default constructor, does nothing so head & tail both will equal NULL
-    }
-
-    // functions for controlling queue
-    void joinQueue(int data); // inserting at end of queue
-    void enqueue(); // deleting / removing first item in queue
-    void printQueue(); // prints entire queue
-    void updateTail(); // refactored function to simplfy layout of code, responsible for ensuring tail is correct
-    bool isEmpty(); // check if queue is empty
-};
-
-// check if queue is empty
-bool Queue::isEmpty(){
-    bool emptyStatus = false;
-    if(head == NULL){
-        emptyStatus = true;
-    }
-    return emptyStatus;
-}
-// refactored function to simplfy layout of code, responsible for ensuring tail is correct
-void Queue::updateTail(){
-    Node * nav = head; // creates temp pointer called nav to head
-    while(nav-> next != NULL){ // nav navigates the queue while it's next pointer is not NULL
-        nav = nav->next; // nav moves to next item in list
-    }
-    tail = nav; // when nav->next == NULL, loop above stops, therefore at last item, which gets set as tail
-}
-
-// inserting at end of queue
-void Queue::joinQueue(int data){ //
-    if(head == NULL){ // if head currently does not exist
-        head = new Node(data); // create head with data
-        tail = head; // head and tail both equal same node, as it is only one currently in list
-    }
-    else{ // if head already exists
-    Node * newNode = new Node(data); // create newNode
-    tail-> next = newNode; // set tail (last) nodes next to be equal to new node 
-    updateTail(); // call to update tail, will become new node
-    }
-}
-
-// deleting / removing first value in queue
-void Queue::enqueue(){
-    Node * nextNode = head-> next; // create pointer to new node, equal to 2nd value in list
-    free(head); // delete first
-    head = nextNode; // move head to be equal to the old 2nd value, as located by pointer
-}
-
-// for printing queue values
-void Queue::printQueue(){
-   Node * nav = head; // creating node for navigating queue
-	   while(nav != NULL){ // moves through entire queue
-      cout << nav->data ; // prints out data
-      if(nav != tail){ // for all but last
-          cout << ","; // print comma 
-      }
-      nav = nav->next; // move to next node, continue while loop
-   }
-   cout << endl; // after all nodes printed, print new line.
+    cout << "end of q" << endl;
 }
 
 // class for Nodes
 class EdgeNode
 {
 public:
-    int vertex; // the adjacency info of each node
+    int vertex;     // the adjacency info of each node
     EdgeNode *next; // pointer to next node in list
-};              // end class initialisation
+};                  // end class initialisation
 
 // adjacency list for each vertex with head as EdgeNode type
 class AdjacencyList
@@ -101,7 +33,6 @@ class Graph
 public:
     int nVerticies;      // number of Vertex's in graph
     AdjacencyList *list; // the actual adjacency list, consisting of AdjacencyList class, and therefore Nodes
-    vector<bool> visited;
     Graph(int num)       // default initialiser
     {
         nVerticies = num;                     // set the number of verticies equal to the number being initialised too
@@ -109,22 +40,21 @@ public:
         for (int i = 0; i < nVerticies; i++)  // going through each item in AdjacencyList
         {
             list[i].head = nullptr; // sets the head of each AdjacencyList item to be equal to NULL, as they contain no values
-            visited.push_back(false);
         }
     }
 
     //graph / adjacencyList controls:
     void addEdge(int from, int to); // adds edge to graph
-    void BFS(int startAt);
     void print();                   // prints out adjacency list
+    void BFS(int currentV);
 };
 
 void Graph::addEdge(int from, int to)
 {
-    EdgeNode *newNode = new EdgeNode();     // creates new instance of node
-    newNode->vertex = to;           // creates a new node, whos vertex is the final destination of the edge (to)
-    newNode->next = nullptr;        // sets the "next" pointer to point to nothing
-    if (list[from].head == nullptr) // if "from" AdjacencyList head node currently has no edges
+    EdgeNode *newNode = new EdgeNode(); // creates new instance of node
+    newNode->vertex = to;               // creates a new node, whos vertex is the final destination of the edge (to)
+    newNode->next = nullptr;            // sets the "next" pointer to point to nothing
+    if (list[from].head == nullptr)     // if "from" AdjacencyList head node currently has no edges
     {
         list[from].head = newNode; // "head" (first edge) of vertex = the new node
     }
@@ -144,8 +74,8 @@ void Graph::print() // function used to print graph
     for (int i = 0; i < this->nVerticies; i++) // loop through each vertex
     {
         EdgeNode *temp = list[i].head; // temp pointer to transverse tree
-        cout << i << " -> ";       // output vertex number
-        while (temp != nullptr)    // loop to transverse edges
+        cout << i << " -> ";           // output vertex number
+        while (temp != nullptr)        // loop to transverse edges
         {
             cout << temp->vertex; // print out edge connecting value
             if (temp->next != nullptr)
@@ -158,25 +88,38 @@ void Graph::print() // function used to print graph
     }
 }
 
-void Graph::BFS(int startAt){
-    for(int i = 0; i < nVerticies; i++){ // when calling, ensure all visited vector is set to false
-        visited[i] = false;
-    }
-    Queue * Q = new Queue; // initialising a queue
-    visited[startAt] = true; 
-    Q->joinQueue(startAt);
-
-    while (!Q->isEmpty())
+void Graph::BFS(int currentV)
+{
+    vector<bool> visited;
+    for (int i = 0; i < nVerticies; i++)
     {
-        startAt = Q->head->data;
-        cout << startAt;
-        Q->enqueue();
-        for (int i = startAt; i < nVerticies; i++)
+        visited.push_back(false);
+    }
+
+    vector<int> queue;
+
+    visited[currentV] = true;
+    queue.push_back(currentV);
+
+    while (!queue.empty())
+    {
+        currentV = queue.front();
+        cout << currentV << " ";
+        queue.erase(queue.begin());
+
+        EdgeNode *temp = list[currentV].head; // temp pointer to transverse tree
+        int i = temp->vertex;
+        while (temp != nullptr) // loop to transverse edges
         {
             if (!visited[i])
             {
                 visited[i] = true;
-                Q->joinQueue(i);
+                queue.push_back(i);
+            }
+            temp = temp->next; // move to next
+            if (temp != nullptr)
+            {
+                i = temp->vertex;
             }
         }
     }
@@ -185,7 +128,7 @@ void Graph::BFS(int startAt){
 int main() // funtion driver
 {
     Graph *AL = new Graph(4); // initialising AdjacenyList with 4 Vertex's
-    AL->addEdge(0, 1);
+    AL->addEdge(0, 1);        // add edges between 0 and 1
     AL->addEdge(0, 2);
     AL->addEdge(1, 2);
     AL->addEdge(2, 0);
@@ -193,4 +136,5 @@ int main() // funtion driver
     AL->addEdge(3, 3);
 
     AL->BFS(2); // print out adjacency list
+    // AL->print();
 }
